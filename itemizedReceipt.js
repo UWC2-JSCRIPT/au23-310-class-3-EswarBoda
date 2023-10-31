e74fa204762c9dc5cda94fbb2a0084fbb1fbf389
EswarBoda <eswarkrao7@gmail.com>
2023-10-30 22:44:07 -0700

    Kotesh Eswar Week 3 code changes

--- itemizedReceipt.js
+++ itemizedReceipt.js
@@ -2,8 +2,21 @@
 // with these properties: {descr, price}
 // i.e. {descr: 'Coke', price: 1.99}
 // function should log each item to the console and log a total price
+// using NumberFormat to print $$ currency sign, instead of using $$ string literal
+let USDollar = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
 
+const logReceipt = (...items) => {
+  let total = 0;
+  items.forEach((item) => {
+    total += item.price;
+    console.log(item.descr + ' - ' +  USDollar.format(item.price));
+  })
 
+  console.log('Total - ' + USDollar.format(total));
+};
 
 // Check
 logReceipt(
@@ -16,3 +29,8 @@ logReceipt(
 // Chips & Salsa - $2.99
 // Sprite - $1.99
 // Total - $10.97
+/*const obj1 = {descr: 'Burrito', price: 5.99}
+const obj2 = {descr: 'Chips & Salsa', price: 2.99}
+const obj3 = {descr: 'Sprite', price: 1.99}
+
+logReceipt(obj1, obj2, obj3);*/
--- phoneNumber.js
+++ phoneNumber.js
@@ -4,8 +4,11 @@
 // '206-333-4444'
 // '206 333 4444'
 // Returns true if valid, false if not valid
-
-
+function testPhoneNumber(phoneNumber) {
+    console.log('Input phone number is: ', phoneNumber);
+    const regex = new RegExp(`^[(]?[0-9]{3}[)]?[-\s ]?[0-9]{3}[-\s ]?[0-9]{4}$`);
+    return regex.test(phoneNumber);
+}
 
 // Explanation of RegExp
 // ^      start of line
@@ -19,9 +22,10 @@
 // $      end of line
 
 // check testPhoneNumber
-console.log(testPhoneNumber('(206) 333-4444')); // should return true
-console.log(testPhoneNumber('(206) 33-4444')); // should return false, missing a digit
-
+console.log('Result of regex match to phone number is: ', testPhoneNumber('(206) 333-4444')); // should return true
+console.log('Result of regex match to phone number is: ', testPhoneNumber('(206) 33-4444')); // should return false, missing a digit
+console.log('Result of regex match to phone number is: ', testPhoneNumber('206-333-4444')); // should return true
+console.log('Result of regex match to phone number is: ', testPhoneNumber('206 333 4444')); // should return true
 
 // 1. Create a function parsePhoneNumber that takes in a phoneNumber string 
 // in one of the above formats.  For this, you can *assume the phone number
@@ -29,8 +33,36 @@ console.log(testPhoneNumber('(206) 33-4444')); // should return false, missing a
 // and run the exec method to capture the area code and remaining part of
 // the phone number.
 // Returns an object in the format {areaCode, phoneNumber}
+const phoneNumberObj = {
+    areaCode: '',
+    phoneNumber: ''
+};
 
+function parsePhoneNumber(phoneNumber) {
+    // Use dynamic regex string based on the inout'd phone number
+    let regexStr = '';
+    if (phoneNumber.substring(0, 1) === '(') {
+        regexStr = /[)]/g;
+    } else {
+        regexStr = /-/g;
+    }
 
+    const regex = new RegExp(regexStr);
+    let result = regex.exec(phoneNumber);  // regex exec method
+    while (result != null) {
+        // Remove leading parentheses for ph.nos with (206) format
+        // This will not affect, if there is no leading parentheses
+        areaCodeWithParentheses = phoneNumber.substring(0, result.index);
+        areaCodeWithoutParentheses = areaCodeWithParentheses.replace('(', '');
+        phoneNumberObj.areaCode = areaCodeWithoutParentheses;
+
+        let phNumWithHyphen = phoneNumber.substring(result.index+1);
+        let phNumWithoutHyphen = phNumWithHyphen.replace('-', '');
+        phoneNumberObj.phoneNumber = phNumWithoutHyphen;
+        return phoneNumberObj;
+        //result = regex.exec(phoneNumber);
+    }
+}
 
 // Check parsePhoneNumber
 console.log(parsePhoneNumber('206-333-4444'));
